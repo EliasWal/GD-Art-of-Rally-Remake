3bfd754effc1e725d8251b4aac01d7d2b1716aa7
EliasWal <iguglee@gmail.com>
2024-03-28 01:56:20 +0200

    update

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -1,44 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public float moveSmoothness;
-    public float rotSmoothness;
-
-    public Vector3 moveOffset;
-    public Vector3 rotOfffset;
-
     public Transform carTarget;
+    public Vector3 offsetPosition;
+    public Vector3 offsetRotation;
 
-    void FixedUpdate()
+    [Range(0.1f, 10f)]
+    public float followSpeed = 5f;
+
+    private Vector3 targetPosition;
+    private Quaternion targetRotation;
+
+    void LateUpdate()
     {
         FollowTarget();
     }
 
     void FollowTarget()
     {
-        HandleMovement();
-        HandleRotation();
+        // Calculate the target position and rotation
+        targetPosition = carTarget.TransformPoint(offsetPosition);
+        targetRotation = Quaternion.LookRotation(carTarget.forward + offsetRotation, carTarget.up);
+
+        // Interpolate the camera's position and rotation towards the target
+        transform.position = Vector3.Lerp(transform.position, targetPosition, followSpeed * Time.deltaTime);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, followSpeed * Time.deltaTime);
     }
-
-    void HandleMovement()
-    {
-        Vector3 targetPos = new Vector3();
-        targetPos = carTarget.TransformPoint(moveOffset);
-
-        transform.position = Vector3.Lerp(transform.position, targetPos, moveSmoothness * Time.deltaTime);
-    }
-
-    void HandleRotation()
-    {
-        var direction = carTarget.position - transform.position;
-        var rotation = new Quaternion();
-
-        rotation = Quaternion.LookRotation(direction + rotOfffset, Vector3.up);
-
-        transform.rotation = Quaternion.Lerp(transform.rotation, rotation, rotSmoothness * Time.deltaTime);
-    }
-
 }
---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,98 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    private float horizontalInput, verticalInput;
+    private float currentSteerAngle, currentBreakForce;
+    private bool isBreaking, isHandBrake;
+
+    // Settings
+    [SerializeField] private float motorForce = 1500f;
+    [SerializeField] private float breakForce = 3000f;
+    [SerializeField] private float handBrakeForce = 8000f; // New
+    [SerializeField] private float maxSteerAngle = 30f;
+
+    // Wheel Colliders
+    [SerializeField] private WheelCollider frontLeftWheelCollider, frontRightWheelCollider;
+    [SerializeField] private WheelCollider rearLeftWheelCollider, rearRightWheelCollider;
+
+    // Wheels
+    [SerializeField] private Transform frontLeftWheelTransform, frontRightWheelTransform;
+    [SerializeField] private Transform rearLeftWheelTransform, rearRightWheelTransform;
+
+    private void FixedUpdate()
+    {
+        GetInput();
+        HandleMotor();
+        HandleSteering();
+        UpdateWheels();
+    }
+
+    private void GetInput()
+    {
+        // Steering Input
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        // Acceleration Input
+        verticalInput = Input.GetAxis("Vertical");
+
+        // Brake Input
+        isBreaking = Input.GetKey(KeyCode.Space);
+
+        // Handbrake Input
+        isHandBrake = Input.GetKey(KeyCode.LeftShift); // Assuming Left Shift as handbrake key
+    }
+
+    private void HandleMotor()
+    {
+        frontLeftWheelCollider.motorTorque = verticalInput * motorForce;
+        frontRightWheelCollider.motorTorque = verticalInput * motorForce;
+        currentBreakForce = isBreaking ? breakForce : 0f;
+        ApplyBreaking();
+    }
+
+    private void ApplyBreaking()
+    {
+        if (isHandBrake)
+        {
+            rearLeftWheelCollider.brakeTorque = handBrakeForce;
+            rearRightWheelCollider.brakeTorque = handBrakeForce;
+        }
+        else
+        {
+            rearLeftWheelCollider.brakeTorque = currentBreakForce;
+            rearRightWheelCollider.brakeTorque = currentBreakForce;
+        }
+
+        // Apply regular brakes to front wheels
+        frontLeftWheelCollider.brakeTorque = currentBreakForce / 2;
+        frontRightWheelCollider.brakeTorque = currentBreakForce / 2;
+    }
+
+    private void HandleSteering()
+    {
+        currentSteerAngle = maxSteerAngle * horizontalInput;
+        frontLeftWheelCollider.steerAngle = currentSteerAngle;
+        frontRightWheelCollider.steerAngle = currentSteerAngle;
+    }
+
+    private void UpdateWheels()
+    {
+        UpdateSingleWheel(frontLeftWheelCollider, frontLeftWheelTransform);
+        UpdateSingleWheel(frontRightWheelCollider, frontRightWheelTransform);
+        UpdateSingleWheel(rearRightWheelCollider, rearRightWheelTransform);
+        UpdateSingleWheel(rearLeftWheelCollider, rearLeftWheelTransform);
+    }
+
+    private void UpdateSingleWheel(WheelCollider wheelCollider, Transform wheelTransform)
+    {
+        Vector3 pos;
+        Quaternion rot;
+        wheelCollider.GetWorldPose(out pos, out rot);
+        wheelTransform.rotation = rot;
+        wheelTransform.position = pos;
+    }
+}
